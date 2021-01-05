55611db656b7af0e9cafd51b8801c4cd348ae24b
Hakan Bali <h.bali@hymnai.com>
2021-01-05 23:22:00 +0000

    First commit of the project which has been generated on start.spring.io

---
+++ src/main/java/com/arkhelais/scottishmunros/ScottishmunrosApplication.java
@@ -0,0 +1,13 @@
+package com.arkhelais.scottishmunros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScottishmunrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScottishmunrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arkhelais/scottishmunros/ScottishmunrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkhelais.scottishmunros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScottishmunrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
