52a6afd673dff45ba73a03789b5d4b6f6c2ad315
Hakan Bali <hakan.bali.uk@gmail.com>
2021-01-12 17:13:47 +0000

    TestBuilder class introduced to generate test objects.

---
+++ src/test/java/com/arkhelais/scottishmunros/util/TestBuilder.java
@@ -0,0 +1,60 @@
+package com.arkhelais.scottishmunros.util;
+
+import static com.arkhelais.scottishmunros.dto.CategoryType.*;
+
+import com.arkhelais.scottishmunros.dto.MunroListResponse;
+import com.arkhelais.scottishmunros.model.Munro;
+import java.util.List;
+
+public class TestBuilder {
+
+  public static Munro createMunro1() {
+    return Munro.builder()
+        .id("1")
+        .category(MUN.name())
+        .name("Sample Munro")
+        .height(1000.0)
+        .gridReference("NN111111")
+        .build();
+  }
+
+  public static Munro createMunro2() {
+    return Munro.builder()
+        .id("2")
+        .category(MUN.name())
+        .name("Example Munro")
+        .height(900.0)
+        .gridReference("NN222222")
+        .build();
+  }
+
+  public static Munro createMunroTop1() {
+    return Munro.builder()
+        .id("11")
+        .category(TOP.name())
+        .name("Sample Munro Top")
+        .height(850.0)
+        .gridReference("NN333333")
+        .build();
+  }
+
+  public static Munro createMunroTop2() {
+    return Munro.builder()
+        .id("12")
+        .category(TOP.name())
+        .name("Example Munro Top")
+        .height(950.0)
+        .gridReference("NN444444")
+        .build();
+  }
+
+  public static MunroListResponse getResponse() {
+    return MunroListResponse.builder()
+        .total(4)
+        .filtered(4)
+        .limit(10)
+        .results(List.of(createMunro1(), createMunro2(), createMunroTop1(), createMunroTop2()))
+        .build();
+  }
+
+}
